a3d810ab250093ce733507b1bcd203ca9aaeb2bb
codedaoneu <thang.nv0912@gmail.com>
2022-09-28 23:50:15 +0700

    add file

---
+++ tumoi.py
@@ -0,0 +1,13 @@
+#------ Làm tool để học từ 
+#------ Học từ nghĩ đến làm tool
+#------ Chức năng
+#-- Các từ đã học được nhập vào 1 file excel.
+#-- file gồm có: # Ngày học từ lần đầu (ngày import dữ liệu vào file excel)
+                 # Từ mới
+                 # Nghĩa tiếng Việt của nó
+# Tool có 3 chế độ: Học tất `--all`, học theo ngày `--date`, học theo khoảng ngày `--range`
+# Sau khi chạy tool, Đếm số từ bạn sẽ học trong lần chạy đó, hiển thị từng từ mới một cách random
+# Sau khi nhập câu trả lời, tool trả ra đáp án để người dùng tự đối chiếu. 1s sau, tool trả ra từ tiếp theo.
+# Cứ như vậy cho đến hết số lượng từ cần học. Cứ mỗi 10 từ, đếm lại số từ chưa học 1 lần.
+# Cảm ơn sau khi đã học xong.
+#*******--------dev trên môi trường ảo nhé----------*******#
